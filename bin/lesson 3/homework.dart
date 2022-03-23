cc0b479a1d102c73be7bf92a951d7e03448e0a75
Ilya Shamaev <ishamaev.is@gmail.com>
2022-03-23 11:59:01 +0400

    home work lesson 3 ilya shamaev

--- bin/lesson 3/homework.dart
+++ bin/lesson 3/homework.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 /// Практика
 // Напишите функцию, которая при заданном числе возвращает массив подмассивов возрастающей длины.
 // Пример:
@@ -7,7 +9,13 @@
 // pyramid(3) => [ [1], [1, 1], [1, 1, 1] ]
 
 pyramid(int n) {
-  // ваш код
+  List<List<int>> list = [];
+  var j = 0;
+  while (j != n) {
+    list.add([for (int i = 0; i <= j; i++) 1]);
+    j += 1;
+  }
+  print(list);
 }
 
 // Напишите функцию, которая получает массив чисел и возвращаете сумму всех положительных.
@@ -15,10 +23,12 @@ pyramid(int n) {
 // [1,-4,7,12] => 1 + 7 + 12 = 20
 
 sumOfPositive(List<int> nums) {
-  // Ваш код
+  nums.removeWhere((element) => element <= 0);
+  var sum = nums.reduce((value, element) => value + element);
+  print(sum);
 }
 
 void main(List<String> arguments) {
-  pyramid(10);
-  sumOfPositive([10, -2, 30, 2]);
+  pyramid(5);
+  sumOfPositive([10, -2, 30, 2, 0, -100, 8]);
 }
